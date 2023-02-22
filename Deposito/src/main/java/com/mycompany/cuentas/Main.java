23fb2ab67e92f3db2f39919d9e26df83cb504110
kevpa <kevpa@DESKTOP-821ULC4>
2023-02-22 23:18:57 +0100

    Proyecto_Deposito una vez finalizado el apartado 1 de la tarea 04

--- Deposito/src/main/java/com/mycompany/cuentas/Main.java
+++ Deposito/src/main/java/com/mycompany/cuentas/Main.java
@@ -12,13 +12,14 @@ public class Main {
     public static void main(String[] args) {
         CCuenta cuenta1;
         double saldoActual;
+        float cantidad = 0;
         
         
         cuenta1 = new CCuenta("Antonio LÃ³pez","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
-        Operativa_cuenta(cuenta1, 0);
+        Operativa_cuenta(cuenta1, cantidad);
     }
 
     private static void Operativa_cuenta(CCuenta cuenta1, float cantidad) {
