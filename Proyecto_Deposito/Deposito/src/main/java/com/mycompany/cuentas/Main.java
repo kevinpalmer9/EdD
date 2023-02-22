1e9ff2feb20c8754391d90aeb4231eae4ced89d4
kevinpalmer9 <kevpalmer_9@hotmail.com>
2023-02-22 23:05:47 +0100

    Proyecto_Deposito
    Proyecto_Deposito una vez finalizado el apartado 1 de la tarea 04

---
+++ Proyecto_Deposito/Deposito/src/main/java/com/mycompany/cuentas/Main.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cuentas;
+
+/**
+ *
+ * @author kevpa
+ */
+public class Main {
+    public static void main(String[] args) {
+        CCuenta cuenta1;
+        double saldoActual;
+        
+        
+        cuenta1 = new CCuenta("Antonio LÃ³pez","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+
+        Operativa_cuenta(cuenta1, 0);
+    }
+
+    private static void Operativa_cuenta(CCuenta cuenta1, float cantidad) {
+        try {
+            cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
